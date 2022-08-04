ba34c3d414a580fea0bf763aeff7111bdd9311a2
mk-hasan <mkhasan6891@gmail.com>
2022-08-05 00:17:55 +0600

    product component add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Home from "./component/Home";
 import Navbar from "./component/Navbar";
+import Products from "./component/Products";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,32 +3,32 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container">
-                    <a class="navbar-brand fw-bold" href="#">eCommerece-Proyojon</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-white navbar-light py-3 shadow-sm">
+                <div className="container">
+                    <a className="navbar-brand fw-bold" href="#">eCommerece-Proyojon</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Products</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Products</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">About</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">About</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Contact</a>
                             </li>
 
                         </ul>
-                        <div className="buttons">
-                            <a href="" className="btn btn-outline-dark"><i className='fa fa-sign-in me-1'></i>Login</a>
-                            <a href="" className="btn btn-outline-dark ms-2"><i className='fa fa-user-plus me-1'></i>Register</a>
-                            <a href="" className="btn btn-outline-dark ms-2"><i className='fa fa-shopping-cart me-1'></i>Cart (0)</a>
+                        <div classNameName="buttons">
+                            <a href="" classNameName="btn btn-outline-dark"><i classNameName='fa fa-sign-in me-1'></i>Login</a>
+                            <a href="" classNameName="btn btn-outline-dark ms-2"><i classNameName='fa fa-user-plus me-1'></i>Register</a>
+                            <a href="" classNameName="btn btn-outline-dark ms-2"><i classNameName='fa fa-shopping-cart me-1'></i>Cart (0)</a>
                         </div>
                     </div>
                 </div>
