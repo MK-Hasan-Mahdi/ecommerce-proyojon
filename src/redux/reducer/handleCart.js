3b3d7796883dfbd75dbe7d0f0d143c3c1d8808d4
mk-hasan <mkhasan6891@gmail.com>
2022-08-07 08:03:03 +0600

    reducer handleCart added

---
+++ src/redux/reducer/handleCart.js
@@ -0,0 +1,19 @@
+const cart = [];
+
+const handleCart = (state = cart, action) => {
+    const product = action.payload;
+    switch (action.type) {
+        case ADDITEM:
+            // check if product is already exist
+            const exist = state.find(x => x.id === product.id);
+            if (exist) {
+                // increase the quantity
+                return state.map(x => x.id === product.id ? { ...x, qty: x.qty + 1 } : x);
+            }
+
+            break;
+
+        default:
+            break;
+    }
+}
