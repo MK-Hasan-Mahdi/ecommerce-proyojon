4aad5db27f2483630f868385172221ebb0517a57
mk-hasan <mkhasan6891@gmail.com>
2022-08-07 07:03:43 +0600

    redux action file created

--- src/component/ProductDetail.jsx
+++ src/component/ProductDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
     const { productId } = useParams();
@@ -15,12 +15,17 @@ const ProductDetail = () => {
             .then(data => setProduct(data))
     }, []);
 
+    const navigate = useNavigate();
+    const navigateToCart = () => {
+        navigate('/cart')
+    }
+
     return (
         <div className='container'>
 
             <div className='row'>
                 <div className="col-md-6">
-                    <img src={image} alt="" />
+                    <img className='img-fluid' src={image} alt="" />
                 </div>
                 <div className="col-md-6">
                     <h4 className="text-uppercase text-black-50">{category}</h4>
@@ -28,7 +33,8 @@ const ProductDetail = () => {
                     <p className='lead fw-bolder'>Rating {rating && rating.rate} <i className='fa fa-star'></i> </p>
                     <h3 className='display-6 fw-bold my-4'>{price}</h3>
                     <p className='lead'>{description}</p>
-                    <button className='btn btn-outline-dark'>Add to Cart</button>
+                    <button className='btn btn-outline-dark px-4 py-2'>Add to Cart</button>
+                    <button onClick={() => { navigateToCart() }} className='btn btn-dark ms-2 px-3 py-2'>Go to Cart</button>
                 </div>
             </div>
         </div>
---
+++ src/redux/action/index.js
@@ -0,0 +1,15 @@
+// for add item to Cart
+export const addCart = (product) => {
+    return {
+        type: 'ADDITEM',
+        payload: product
+    }
+}
+
+// for delete item from Cart
+export const delCart = (product) => {
+    return {
+        type: 'DELITEM',
+        payload: product
+    }
+}
